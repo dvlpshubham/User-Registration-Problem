760cacd712434ee0ff90ba14f9b0a5fac6ff037a
Shubham Raj <shubhamaryan805@gmail.com>
2022-12-01 22:37:30 +0530

    Added : Logic to match password rule 1 with regex

--- src/main/java/com/javaregex/UserRegistrationMain.java
+++ src/main/java/com/javaregex/UserRegistrationMain.java
@@ -18,6 +18,7 @@ public class UserRegistrationMain {
             System.out.println("Enter 2 : To validate Last Name ");
             System.out.println("Enter 3 : To validate E-mail ");
             System.out.println("Enter 4 : To validate Mobile Number ");
+            System.out.println("Enter 5 : To validate Password Rule 1 ");
 
             switch (scanner.nextInt()) {
                 case 0:
@@ -37,6 +38,10 @@ public class UserRegistrationMain {
 
                 case 4:
                     UserRegistration.validMobileNumber();
+                    break;
+
+                case 5:
+                    UserRegistration.validPassRule1();
                     break;
             }
         }
